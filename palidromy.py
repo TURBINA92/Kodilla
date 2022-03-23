611ffa2aa8225aca3fc8d4cd1ada64e95e3badcd
TURBINA92 <96655949+TURBINA92@users.noreply.github.com>
2022-03-23 19:02:20 +0100

    Palidrome test

---
+++ palidromy.py
@@ -0,0 +1,15 @@
+def funkcja_test():
+    print("funkcja")
+
+funkcja_test()
+=======
+def isplindrome(word):
+    if word[0==-1]:
+        return True
+    else:
+        return False
+
+word= "ada"
+wynik= isplindrome(word)
+print(wynik)
+
